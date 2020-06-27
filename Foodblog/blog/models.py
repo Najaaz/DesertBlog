bf389b4092dfac66757fff2bcb786d7fe95c3641
Najaaz Nabhan <najaaznabhan@gmail.com>
2020-06-27 20:30:53 +0530

    Front end is somewhat completed, typing animation is done

--- Foodblog/blog/models.py
+++ Foodblog/blog/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.CharField()
-    image = models.ImageField(blank=True)
+    content = models.CharField(max_length=400)
+    image = models.ImageField(blank=True,)
     date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
@@ -15,7 +15,10 @@ class Post(models.Model):
 class Comments(models.Model):
     post_connection = models.ForeignKey(Post, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    content = models.CharField()
+    content = models.CharField(max_length=300)
 
     def __str__(self):
         return f"({self.post_connection}) =>{self.name}"
+
+    class Meta:
+        verbose_name_plural = "Comments"
